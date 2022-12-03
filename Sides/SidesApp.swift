24adb120b07e76f15708db3222ae607197bf512e
Diego Herrera Redondo <diegoherred@gmail.com>
2022-12-03 19:02:14 +0100

    Initial Commit

---
+++ Sides/SidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidesApp.swift
+//  Sides
+//
+//  Created by Diego Herrera Redondo on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
